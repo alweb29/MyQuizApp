4ab06d07b1df645e1ae93b86d0c92ff469e9166b
alweb29 <albertow55@gmail.com>
2023-09-21 11:27:37 +0200

    Implemented selection of each button in the question section

--- app/src/main/java/com/example/alweb29/test/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/alweb29/test/QuizQuestionsActivity.kt
@@ -1,19 +1,30 @@
 package com.example.alweb29.test
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.alweb29.test.databinding.ActivityQuizQuestionsBinding
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener{
+
+    private var mCurrentPosition : Int = 1
+    private var mQuestionList : ArrayList<Question>? = null
+    private var mSelectedQuestionPosition : Int = 0
+    
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityQuizQuestionsBinding
@@ -27,9 +38,13 @@ class QuizQuestionsActivity : AppCompatActivity() {
     private var tvOptionTwo : TextView? = null
     private var tvOptionThree : TextView? = null
     private var tvOptionFour : TextView? = null
+    private var btnSubmit : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityQuizQuestionsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         progressBar = findViewById(R.id.progress_bar)
         tvProgressBar = findViewById(R.id.tv_progress)
@@ -39,33 +54,111 @@ class QuizQuestionsActivity : AppCompatActivity() {
         tvOptionTwo = findViewById(R.id.tv_option_two)
         tvOptionThree = findViewById(R.id.tv_option_three)
         tvOptionFour = findViewById(R.id.tv_option_four)
+        btnSubmit = findViewById(R.id.btn_submit)
 
-        val questionsList = Constants.getQuestions()
-        Log.i("There are", "${questionsList.size}")
-        for (i in questionsList){
-            Log.e("Question", i.question)
-        }
+        tvOptionOne?.setOnClickListener(this)
+        tvOptionTwo?.setOnClickListener(this)
+        tvOptionThree?.setOnClickListener(this)
+        tvOptionFour?.setOnClickListener(this)
+        btnSubmit?.setOnClickListener(this)
 
+        mQuestionList = Constants.getQuestions()
+        setQuestion()
 
-        var currentPosition = 1
-        var question :Question = questionsList[currentPosition - 1]
+    }
+
+    private fun setQuestion() {
+
+        var question: Question = mQuestionList!![mCurrentPosition - 1]
 
         ivImage?.setImageResource(question.image)
-        tvProgressBar?.text = "$currentPosition/${progressBar?.max}"
+        tvProgressBar?.text = "$mCurrentPosition/${progressBar?.max}"
         tvQuestion?.text = question.question
         tvOptionOne?.text = question.optionOne
         tvOptionTwo?.text = question.optionTwo
         tvOptionThree?.text = question.optionThree
-        tvOptionFour?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
 
 
-        binding = ActivityQuizQuestionsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        if (mCurrentPosition == mQuestionList!!.size){
+            btnSubmit?.text ="Finish"
+        }else{
+            btnSubmit?.text ="Next"
+        }
+    }
+
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+        tvOptionOne?.let {
+            options.add(0, it)
+        }
+        tvOptionTwo?.let {
+            options.add(1, it)
+        }
+        tvOptionThree?.let {
+            options.add(2, it)
+        }
+        tvOptionFour?.let {
+            options.add(3, it)
+        }
+
+        for (option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border
+            )
+        }
+    }
+
+    private fun selectedOptionView(tv:TextView, selectedOptionNumber: Int){
+        defaultOptionsView()
+
+        mSelectedQuestionPosition = selectedOptionNumber
+        tv.setTextColor(Color.parseColor("366A43"))
+        tv.setTypeface(tv.typeface, Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_bg
+        )
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_quiz_questions)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.tv_option_one->{
+                tvOptionOne?.let {
+                    selectedOptionView(it, 1)
+                }
+            }
+
+            R.id.tv_option_two->{
+                tvOptionTwo?.let {
+                    selectedOptionView(it, 2)
+                }
+            }
+
+            R.id.tv_option_three->{
+                tvOptionThree?.let {
+                    selectedOptionView(it, 3)
+                }
+            }
+
+            R.id.tv_option_four->{
+                tvOptionFour?.let {
+                    selectedOptionView(it, 4)
+                }
+            }
+
+            R.id.btn_submit->{
+                //TODO "btn submit implement"
+            }
+        }
     }
 }
